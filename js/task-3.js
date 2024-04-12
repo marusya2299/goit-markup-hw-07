8467305ec3b13721f57bfbc09e2cc914c4fbab6a
marusya2299 <marsmaryna@.com>
2024-04-12 16:24:01 +0200

    /

--- js/task-3.js
+++ js/task-3.js
@@ -1,2 +1,16 @@
 'use strict';
 
+const inputName = document.querySelector('#name-input');
+const outputName = document.querySelector('#name-output');
+
+console.log(outputName.textContent);
+
+inputName.addEventListener("input", (event) => {
+
+    if (inputName.textContent.trim() === '') {
+        outputName.textContent = event.target.value.trim();
+    }
+    else {
+        outputName.textContent = 'Anonymous';
+    }
+})
